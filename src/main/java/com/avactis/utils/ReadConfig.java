6e35f4e8ccf4449ea37cd4d984edcc1c3f192c63
Vedika Gaikwad <vedikagaikwad2703@gmail.com>
2024-08-01 00:10:12 +0530

    Added ReadConfig class

--- src/main/java/com/avactis/utils/ReadConfig.java
+++ src/main/java/com/avactis/utils/ReadConfig.java
@@ -1,5 +1,26 @@
 package com.avactis.utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ReadConfig {
+
+	Properties properties;
+
+	public ReadConfig() {
+		properties = new Properties();
+		try {
+			FileInputStream input = new FileInputStream("src/main/resources/config.properties");
+			properties.load(input);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getProperty(String key) {
+		return properties.getProperty(key);
+	}
 
 }
